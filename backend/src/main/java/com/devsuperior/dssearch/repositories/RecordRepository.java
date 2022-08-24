e677efd9097b0aa9c4f5ef85ee87837200021f16
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-08-24 16:30:41 -0300

    Security Config

--- backend/src/main/java/com/devsuperior/dssearch/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dssearch/repositories/RecordRepository.java
@@ -11,10 +11,10 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RecordRepository extends JpaRepository<Record, Long> {
+public interface RecordRepository extends JpaRepository<com.devsuperior.dssearch.entities.Record, Long> {
  
-	@Query("SELECT obj FROM Record obj WHERE "
-			+ "(:min IS NULL OR obj.moment >= :min) AND "
-			+ "(:max IS NULL OR obj.moment <= :max) ")
-	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
+	@Query("SELECT obj FROM com.devsuperior.dssearch.entities.Record obj WHERE "
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max) ")
+	Page<com.devsuperior.dssearch.entities.Record> findByMoments(Instant min, Instant max, Pageable pageable);
 }
