7cc222f413f30fea8d8e4f80d50030478df4bb0c
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-08-25 21:29:27 -0300

    Created page home

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+
 import './styles.css';
 
 export default function Header() {
-  return (
-    <header className="main-header">
-        <Logo />
-        <div className="logo-text">
-            <span className="logo-text-1">Big Game </span>
-            <span className="logo-text-2">Survey</span>
-        </div>
-    </header>
-  );
+    return (
+        <header className="main-header">
+            <Logo />
+            <div className="logo-text">
+                <span className="logo-text-1">Big Game </span>
+                <span className="logo-text-2">Survey</span>
+            </div>
+        </header>
+    );
 };
